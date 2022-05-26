4c9900bd5e336dbec50706550daa61eee72aaaad
Brahian Torres <brahiant0@gmail.com>
2022-05-26 18:07:42 -0500

    primer commit

---
+++ src/main/java/com/Iglesia/proyectoEmanuel/JuventudVisibleApplication.java
@@ -0,0 +1,13 @@
+package com.Iglesia.proyectoEmanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuventudVisibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuventudVisibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Iglesia/proyectoEmanuel/JuventudVisibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Iglesia.proyectoEmanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuventudVisibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
